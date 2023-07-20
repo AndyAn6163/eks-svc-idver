d2905477dfc7d551f00c84149afc220af47b4b0a
Andy An <lanews580@gmail.com>
2023-07-20 23:54:51 +0800

    First commit

---
+++ src/main/java/com/eks/svc/idver/EksSvcIdverApplication.java
@@ -0,0 +1,13 @@
+package com.eks.svc.idver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksSvcIdverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksSvcIdverApplication.class, args);
+	}
+
+}
